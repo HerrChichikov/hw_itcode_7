02be057048d7b02e943c8b64c2793df252dfe3a8
herrchichikov <richardzagidullin69@gmail.com>
2023-07-20 21:07:46 +0500

    Добавил лукапы в листинг QuerySet

--- core/models.py
+++ core/models.py
@@ -1,11 +1,12 @@
+from datetime import date
+
 from django.db import models
 
 
 class Home(models.Model):
     street = models.CharField(max_length=255, blank=True)
     house = models.CharField(max_length=255, blank=True)
-    city = models.CharField(max_length=255, blank=True)
-    date = models.DateTimeField(auto_created=True, null=True, blank=True)
+    city = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return f"Home: {self.get_full_address()}"
